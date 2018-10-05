1f683d9d6f6434dabad98b68e5f9cec98a553419
Bryce Hasshaw <bhasshaw@gmail.com>
2018-10-04 19:15:26 -0700

    file changes

---
+++ src/components/top-nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from './nav-bar';
+
+export default function Header(props) {
+    return (
+        <header>
+            <h1>Hot || Cold</h1>
+        </header>
+    );
+}
+
