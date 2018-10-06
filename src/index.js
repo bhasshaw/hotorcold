7a0f2aa618869557d8369b4897b1e1dba6ab3718
Bryce Hasshaw <bhasshaw@gmail.com>
2018-10-06 16:51:08 -0700

    fixing errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './components/game';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
